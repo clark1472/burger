8ca3fdcdca4da33180339b7d2767e86326befd1b
Salsa <>
2019-05-20 13:16:12 -0700

    tweeking files connecting each other

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-    id INTEGER(100) AUTO_INCREMENT NOT NULL,
-    burger_name varchar(100),
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+USE burger_db;
 INSERT INTO burgers (id, burger_name, devoured) values (1, "chicken bacon ranch", false);
 INSERT INTO burgers (id, burger_name, devoured) values (2, "hamburger cheese pickel", false);
 INSERT INTO burgers (id, burger_name, devoured) values (3, "turkey ham roastbeef lettuce tomato", false);
