8bc3f72f71b183db41219db0b77e85d098b273f5
Salsa <>
2019-05-28 14:18:46 -0700

    tweating css to my liking

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burger_db;
 INSERT INTO burgers (burger_name, devoured) VALUES (1, "chicken bacon ranch", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES (2, "hamburger cheese pickel", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES (2, "hamburger cheese pickle", FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES (3, "turkey ham roastbeef lettuce tomato", FALSE);
