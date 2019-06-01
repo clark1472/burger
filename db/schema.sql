9c4b58ed6879dc37330094437fa0dc1de2c9621d
Salsa <>
2019-06-01 09:56:52 -0700

    make it better

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,12 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
+USE j9ln3qu1f1y50dxa;
 
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT FALSE,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
